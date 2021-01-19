428ee9cadbace9fc601a2dcad7c48e9858d6d317
amimuj <amir.mu@gmail.com>
2021-01-19 23:26:49 +0100

    created first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Greeting} from './components/greeting/Greeting'
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+      <Greeting />
+    </>
   );
 }
 
---
+++ src/components/greeting/Greeting.tsx
@@ -0,0 +1,15 @@
+import './Greeting.css'
+
+const name = "Amir"
+
+export const Greeting = () => {
+    return (
+        <h1 className="title">Hello {name}! awsome website</h1>
+    )
+}
+
+/* export default function Greeting () {
+    <h1>Hello awsome website</h1>
+
+}
+*/
